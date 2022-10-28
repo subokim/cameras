69ae4b9afa3949464c8bafa5c3e7c36bc4d9c481
kimsubo <kimsubo@gmail.com>
2022-10-28 18:20:26 +0900

    cyclinder

--- js/scene/Main_Page.js
+++ js/scene/Main_Page.js
@@ -19,17 +19,23 @@ function (
 		GLOBAL.scene = scene;
 		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 		renderer = new THREE.WebGLRenderer(); 
+		
 		renderer.setSize( window.innerWidth, window.innerHeight ); 
 		document.getElementById("WebGL_space").appendChild( renderer.domElement );
+
 		var controls = new THREE.OrbitControls( camera, renderer.domElement );
+		
 		var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 		directionalLight.position.set(50,10,50);
 		scene.add( directionalLight );
+		
 		var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 1 );
 		directionalLight2.position.set(-50,-10,-50);
 		scene.add( directionalLight2 );
+		
 		var gridHelper = new THREE.GridHelper( 20, 20 );
 		scene.add( gridHelper );
+		
 		var axesHelper = new THREE.AxesHelper( 5 );
 		scene.add( axesHelper );
 
@@ -45,12 +51,18 @@ function (
 		//var cube = new THREE.Mesh( geometry, material );
 		//character.add( cube );
 
-		var geometry = new THREE.ConeGeometry( 0.5, 1.5, 32 );
-		var material = new THREE.MeshPhongMaterial( {color: 0xffff00} );
-		var cone = new THREE.Mesh( geometry, material );
+		//var geometry = new THREE.ConeGeometry( 0.5, 1.5, 32 );
+		//var material = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+		//var cone = new THREE.Mesh( geometry, material );
+		//character.add( cone );
+
+		var geometry = new THREE.CylinderGeometry( 0.5, 0.5, 1, 32, 10 );
+		var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+		var cyclinder = new THREE.Mesh( geometry, material );
+		character.add( cyclinder );
 		
 		//cube.position.x = 1;
-		cone.rotation.x = - 90 * Math.PI/180;
+		//cone.rotation.x = - 90 * Math.PI/180;
 		
 		//cube.position.y = 1;
 		//cube.rotation.y = 10 * Math.PI/180;
@@ -58,14 +70,15 @@ function (
 		//cube.position.z = 1;
 		//cube.rotation.z = 10 * Math.PI/180;
 
-		character.add( cone );
 		scene.add( character );
 
 		character.matrixAutoUpdate = false;
+
 		var position = new THREE.Vector3(3, 3, 3);
 		character.matrix.setPosition(position);
+
 		var eye = position.clone();
-		var target = new THREE.Vector3(-3, -3, -10);
+		var target = new THREE.Vector3(0, 0, 0);
 		var up = new THREE.Vector3(0, 1, 0);
 		character.matrix.lookAt(eye, target, up);
 
